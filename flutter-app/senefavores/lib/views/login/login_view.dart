76de8a60b872217d01f95de5288d273ec06b7dd0
rooot <miguelagc@outlook.es>
2025-03-10 20:20:33 -0500

    change to 1_8

--- flutter-app/senefavores/lib/views/login/login_view.dart
+++ flutter-app/senefavores/lib/views/login/login_view.dart
@@ -1,9 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:uni_links/uni_links.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
