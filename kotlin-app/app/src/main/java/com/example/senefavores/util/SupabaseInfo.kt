bb03a434a43507a40776e66650a724aaecc66cb0
David Perez <d.perezc23@uniandes.edu.co>
2025-03-14 20:45:12 -0500

    Crear favor finalizado, #TODO: LocacionFix, Backend-Push

---
+++ kotlin-app/app/src/main/java/com/example/senefavores/util/SupabaseInfo.kt
@@ -0,0 +1,6 @@
+package com.example.senefavores.util
+
+object SupabaseConfig {
+    const val SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlYnVtemN4dHR5cXVvcmhpaWNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2NDM1MDQsImV4cCI6MjA1NzIxOTUwNH0.PiAnATAnWk_7Brz6XzZqQMkaCoGOItFGKhy1EZ8OnVg"
+    const val SUPABASE_URL = "https://kebumzcxttyquorhiicf.supabase.co"
+}
